2e69558f23202ee86b71ddc90d1ff7324a62ffb3
hawkeyephil <hawkeyephil@icloud.com>
2023-04-22 15:24:18 -0400

    Gathers Necessary Data from User for Repo Creation

--- src/label.java
+++ src/label.java
@@ -2,11 +2,7 @@ package src;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
-import javax.imageio.ImageIO;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
